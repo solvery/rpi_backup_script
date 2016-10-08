f27740095ade4e662396dfdeef09374dc532632b
solvery@163.com <solvery@163.com>
2016-10-08 17:07:06 +0800

    rpi_backup_script

--- rpi_img_mount.sh
+++ rpi_img_mount.sh
@@ -1,10 +1,9 @@
 #/bin/bash
 
-basedir=/workspace/d/
-size=4000
+img_file=$1
 
 # Set the partition variables
-loopdevice=`losetup -f --show ${basedir}/rpi2_backup.img`
+loopdevice=`losetup -f --show ${img_file}`
 device=`kpartx -va $loopdevice| sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
 device="/dev/mapper/${device}"
 bootp=${device}p1
@@ -20,5 +19,4 @@ mkfs.ext4 $rootp
 mkdir -p bootp root
 mount $bootp bootp
 mount $rootp root
-
 
